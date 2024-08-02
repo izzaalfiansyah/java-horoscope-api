4b16fd0fe7a5b5b2f1c552b72bfcf359df30273b
Muhammad Izza Alfiansyah <iansyah724@gmail.com>
2024-08-02 08:34:13 +0700

    add cek nama from primbon

--- src/cekartinama.ts
+++ src/cekartinama.ts
@@ -50,6 +50,45 @@ const getFromCekArtiNama = async (nama: string) => {
   return items;
 };
 
+const getFromPrimbon = async (nama: string) => {
+  const listNama = nama.split(" ");
+
+  let items: {
+    arti: string;
+    detail: string;
+  }[] = [];
+
+  for (let i = 0; i < listNama.length; i++) {
+    const n = listNama[i];
+
+    const response = await axios.get(
+      `https://www.primbon.com/arti_nama.php?nama1=${n}&proses=1`
+    );
+
+    const $ = cheerio.load(response.data);
+
+    try {
+      const body = $("#body").text().split(`Nama ${n}`);
+      const content = body[1];
+      const arti = content.split(".")[0].replace(", memiliki arti: ", "");
+      const detail = content
+        .split(arti)[1]
+        .replace(/\n/gi, "")
+        .slice(1)
+        .split("Nama:")[0];
+
+      items.push({
+        arti,
+        detail,
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  return items;
+};
+
 router.get("/", async (req, res) => {
   const nama: string | null = req.query["nama"] as string;
 
@@ -60,10 +99,12 @@ router.get("/", async (req, res) => {
   }
 
   const items = await getFromCekArtiNama(nama);
+  const primbon = await getFromPrimbon(nama);
 
   res.send({
     success: true,
     items,
+    primbon,
   });
 });
 

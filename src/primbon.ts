a1eb49b7022bcab352933968513d75df03f34e7c
Muhammad Izza Alfiansyah <iansyah724@gmail.com>
2024-08-04 14:04:37 +0700

    fixed error on different title

--- src/primbon.ts
+++ src/primbon.ts
@@ -58,24 +58,30 @@ const getPrimbon = async (props: { tgl: number; bln: number; thn: number }) => {
     sifat_wuku = $(el)
       .html()
       ?.split("Wuku tersebut, adalah:")[1]
+      .split("Di bawah ini")[0]
       .split("Daftar ini")[0]
       .trim();
 
     keadaan_umum = $(el)
       .html()
       .split("KEADAAN UMUM")[1]
-      .split("KEADAAN ALAM SEMESTA")[0]
-      .trim();
-
-    keadaan_alam_semesta = $(el)
-      .html()
-      .split("KEADAAN ALAM SEMESTA")[1]
+      ?.split("KEADAAN ALAM SEMESTA")[0]
+      .split("ALAM SEMESTA")[0]
       .split("KEADAAN FISIK")[0]
       .trim();
 
-    keadaan_fisik = $(el)
-      .html()
-      .split("KEADAAN FISIK")[1]
+    keadaan_alam_semesta =
+      $(el)
+        .html()
+        ?.split("ALAM SEMESTA")[1]
+        ?.split("KEADAAN FISIK")[0]
+        .split("POSTUR TUBUH")[0]
+        .trim() || "<\br>Tidak diketahui";
+
+    keadaan_fisik = (
+      $(el).html()?.split("KEADAAN FISIK")[1] ||
+      $(el).html().split("POSTUR TUBUH")[1]
+    )
       .split("KEADAAN MASA KANAK-KANAK")[0]
       .trim();
 
@@ -85,15 +91,17 @@ const getPrimbon = async (props: { tgl: number; bln: number; thn: number }) => {
       .split("KEADAAN MASA REMAJA")[0]
       .trim();
 
-    keadaan_masa_remaja = $(el)
-      .html()
-      .split("KEADAAN MASA REMAJA")[1]
+    keadaan_masa_remaja = (
+      $(el).html()?.split("KEADAAN MASA REMAJA")[1] ||
+      $(el).html().split("KEADAAN MASA DEWASA")[1]
+    )
       .split("CIRI KHAS")[0]
       .trim();
 
-    ciri_khas = $(el)
-      .html()
-      .split("CIRI KHAS")[1]
+    ciri_khas = (
+      $(el).html()?.split("CIRI KHAS YANG MENCOLOK")[1] ||
+      $(el).html().split("CIRI KHAS")[1]
+    )
       .split("IKATAN PERSAHABATAN")[0]
       .trim();
 
@@ -103,22 +111,25 @@ const getPrimbon = async (props: { tgl: number; bln: number; thn: number }) => {
       .split("KEADAAN KESEHATAN")[0]
       .trim();
 
-    keadaan_kesehatan = $(el)
-      .html()
-      .split("KEADAAN KESEHATAN")[1]
-      .split("PEKERJAAN YANG COCOK")[0]
-      .trim();
+    keadaan_kesehatan =
+      $(el)
+        .html()
+        ?.split("KEADAAN KESEHATAN")[1]
+        ?.split("PEKERJAAN YANG COCOK")[0]
+        .trim() || "</br>Tidak diketahui";
 
-    saat_yang_tepat = $(el)
-      .html()
-      .split("SAAT YANG TEPAT")[1]
+    saat_yang_tepat = (
+      $(el).html()?.split("SAAT YANG TEPAT")[1] ||
+      $(el).html()?.split("SAAT YANG BAIK")[1] ||
+      $(el).html()?.split("HARI JAYA")[1]
+    )
       .split("HOBI")[0]
       .trim();
 
-    jodoh = $(el)
-      .html()
-      .split("JODOH")[1]
-      .split("BATU PERMATA YANG COCOK")[0]
+    jodoh = (
+      $(el).html()?.split("JODOH")[1] || $(el).html().split("JODO PINASTI")[1]
+    )
+      .split("BATU PERMATA")[0]
       .trim();
   });
 
